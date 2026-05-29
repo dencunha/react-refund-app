9ab8b5a4a0ebcdfe83a320160e88c345490aabec
Blenda Cunha <blenda.cunha13@gmail.com>
2026-05-29 20:59:28 -0300

    feat: finalize manager dashboard view with server side filtering and pagination

---
+++ src/dtos/categories.d.ts
@@ -0,0 +1,7 @@
+enum CategoriesAPIEnum {
+    Food = "food",
+    Transport = "transport",
+    Others = "others",
+    Services = "services",
+    Accommodation = "accommodation"
+}
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { AxiosError } from "axios";
+
+import { api } from "../services/api";
 
 import searchSvg from "../assets/search.svg";
 import { CATEGORIES } from "../utils/categories";
@@ -9,23 +12,46 @@ import { Button } from "../components/Button";
 import { Pagination } from "../components/Pagination";
 import { RefundItem, type RefundItemProps } from "../components/RefundItem";
 
-const REFUND_EXAMPLE = {
-  id: "123",
-  name: "John Doe",
-  category: "Transporte",
-  amount: formatCurrency(123.45),
-  categoryImg: CATEGORIES["transport"].icon,
-};
+
+const PER_PAGE = 5
 
 export function Dashboard() {
   const [name, setName] = useState(""); 
   const [page, setPage] = useState(1);
-  const [totalOfPages, setTotalOfPages] = useState(10);
-  const [refunds, setRefunds] = useState<RefundItemProps[]>([REFUND_EXAMPLE]);
+  const [totalOfPages, setTotalOfPages] = useState(0);
+  const [refunds, setRefunds] = useState<RefundItemProps[]>([]);
 
-  function fetchRefunds(e: React.FormEvent) {
-    e.preventDefault();
-    console.log(name);
+  async function fetchRefunds() {
+    try {
+      const response = await api.get<RefundsPaginationAPIResponse>(
+        `/refunds?name=${name.trim()}&page=${page}&perPage=${PER_PAGE}`
+      )
+
+      setRefunds(
+        response.data.refunds.map((refund) => ({
+          id: refund.id,
+          name: refund.user.name,
+          description: refund.name,
+          amount: formatCurrency(refund.amount),
+          categoryImg: CATEGORIES[refund.category].icon,
+        }))
+      )
+
+      setTotalOfPages(response.data.pagination.totalPages)
+   } catch (error) {
+    console.error(error);
+
+    if (error instanceof AxiosError) {
+      return alert(error.response?.data.message)
+    }
+
+    alert ("Não foi possível carregar")
+    }
+  }
+
+  function onSubmit(e: React.FormEvent){
+    e.preventDefault()
+    fetchRefunds()
   }
 
   function handlePagination(action: "next" | "previous") {
@@ -39,12 +65,16 @@ export function Dashboard() {
     });
   }
 
+  useEffect(() => {
+    fetchRefunds()
+  }, [page])
+
   return (
     <div className="bg-gray-500 rounded-xl p-10 md:min-w-[768px]">
       <h1 className="text-gray-100 font-bold text-xl flex-1">Solicitações</h1>
 
       <form
-        onSubmit={fetchRefunds}
+        onSubmit={onSubmit}
         className="flex flex-1 items-center justify-between pb-6 border-b-[1px] border-b-gray-400 md:flex-row gap-2 mt-6"
       >
         <Input
