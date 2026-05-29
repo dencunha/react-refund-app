9fe82e42ddd302d2b61cfdc79377ed8c88a163bc
Blenda Cunha <blenda.cunha13@gmail.com>
2026-05-29 20:58:35 -0300

    feat: introduce AuthContext and hook with localStorage session persistence

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from "react";
+import { createContext } from "react";
+import { api } from "../services/api";
+
+type AuthContext = {
+  isLoading: boolean;
+  session: null | UserAPIResponse;
+  save: (data: UserAPIResponse) => void;
+  remove: () => void;
+};
+
+const LOCAL_STORAGE_KEY = "@refund";
+
+export const AuthContext = createContext({} as AuthContext);
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [session, setSession] = useState<null | UserAPIResponse>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  function save(data: UserAPIResponse) {
+    localStorage.setItem(
+      `${LOCAL_STORAGE_KEY}:user`,
+      JSON.stringify(data.user),
+    );
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token);
+
+    api.defaults.headers.common["Authorization"] = `Bearer ${data.token}`
+
+    setSession(data);
+  }
+
+  function remove() {
+    setSession(null);
+
+    localStorage.removeItem(`${LOCAL_STORAGE_KEY}:user`);
+    localStorage.removeItem(`${LOCAL_STORAGE_KEY}:token`);
+
+    window.location.href = "/";
+  }
+
+  function loadUser() {
+    const user = localStorage.getItem(`${LOCAL_STORAGE_KEY}:user`);
+    const token = localStorage.getItem(`${LOCAL_STORAGE_KEY}:token`);
+
+    if (user && token) {
+    api.defaults.headers.common["Authorization"] = `Bearer ${token}`
+
+      setSession({
+        user: JSON.parse(user),
+        token,
+      });
+    }
+
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ session, save, isLoading, remove }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
