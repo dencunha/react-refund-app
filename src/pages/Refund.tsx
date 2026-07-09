2dec1ce8af0a0f8fe96a7f67afae29f2fd27ac4c
Blenda Cunha <blenda.cunha13@gmail.com>
2026-07-09 10:57:09 -0300

    fix: Changes the receipt URL to point to the dynamic API.

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -163,7 +163,7 @@ export function Refund() {
 
       {(params.id && fileURL) ? (
         <a
-          href={`http://localhost:3333/uploads/${fileURL}`}
+          href={`${api.defaults.baseURL}/uploads/${fileURL}`}
           target="_blank"
           className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 hover:opacity-70 transition ease-linear"
         >
